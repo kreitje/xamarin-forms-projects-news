f5113a99a1cade0b8d8367f141dc6564261e1473
Jeff Kreitner <kreitje@gmail.com>
2021-04-21 18:48:17 -0400

    Scaffold  default views

---
+++ News/Views/ArticleView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace News.Views
+{
+    public partial class ArticleView : ContentPage
+    {
+        public ArticleView()
+        {
+            InitializeComponent();
+        }
+    }
+}
